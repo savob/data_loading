9dafe6b1ea607bae5b7064d89037af7bbd353669
Savo Bajic <bajic.savo@gmail.com>
2024-02-21 22:08:49 -0500

    Converted to non-depreciated functions

--- firmware/data_loading/src/main.cpp
+++ firmware/data_loading/src/main.cpp
@@ -7,15 +7,17 @@
 const pin_size_t R_IN = 26;
 const pin_size_t L_IN = 27;
 
-arduinoFFT FFT = arduinoFFT(); // Create FFT object
-
 const uint16_t samples = 128;
+const double samplingFrquency = 25641;
 double vReal_R[samples];
 double vImag_R[samples];
 double vReal_L[samples];
 double vImag_L[samples];
 int16_t wave_R[samples];
 int16_t wave_L[samples];
+
+arduinoFFT FFTright = arduinoFFT(vReal_R, vImag_R, samples, samplingFrquency);
+arduinoFFT FFTleft = arduinoFFT(vReal_L, vImag_L, samples, samplingFrquency);
 
 void setup() {
   Serial.begin(115200);
@@ -38,14 +40,14 @@ void loop() {
     vImag_L[i] = 0;
   }
   // FFT Calculation  (33ms)
-  FFT.Windowing(vReal_R, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
-  FFT.Windowing(vReal_L, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+  FFTright.Windowing(FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+  FFTleft.Windowing(FFT_WIN_TYP_HAMMING, FFT_FORWARD);
 
-  FFT.Compute(vReal_R, vImag_R, samples, FFT_FORWARD);
-  FFT.Compute(vReal_L, vImag_L, samples, FFT_FORWARD);
+  FFTright.Compute(FFT_FORWARD);
+  FFTleft.Compute(FFT_FORWARD);
 
-  FFT.ComplexToMagnitude(vReal_R, vImag_R, samples);
-  FFT.ComplexToMagnitude(vReal_L, vImag_L, samples);
+  FFTright.ComplexToMagnitude();
+  FFTleft.ComplexToMagnitude();
   // Magnitude is in `vReal_x` arrays
 
   delay(1);
