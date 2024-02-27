fa6916e6db9e06136033bb5616f585d627b2233b
Savo Bajic <bajic.savo@gmail.com>
2024-02-27 02:15:50 -0500

    Added CAP1206 to main loop
    Also changed indent on `main.cpp` from two to four spaces for consistency with other files.

--- firmware/data_loading/src/main.cpp
+++ firmware/data_loading/src/main.cpp
@@ -7,38 +7,42 @@
 TwoWire i2cBus(12, 13);
 
 IS31FL3236 drivers[] = {
-  IS31FL3236(0x3C, 15, &i2cBus),
-  IS31FL3236(0x3F, 16, &i2cBus)
+    IS31FL3236(0x3C, 15, &i2cBus),
+    IS31FL3236(0x3F, 16, &i2cBus)
 };
 
-//Cap1206 touch(&i2cBus);
+Cap1206 touch(&i2cBus);
 
 void setup() {
-  Serial.begin(9600);
-  //while (!Serial) delay(10); // Wait for USB to open for debug messages
-  
-  Serial.println("STARTING DATA BOARD....");
+    Serial.begin(9600);
+    //while (!Serial) delay(10); // Wait for USB to open for debug messages
+    
+    Serial.println("STARTING DATA BOARD....");
 
-  initializeLED(drivers);
-  Serial.println("LED Driver settings selected");
+    initializeLED(drivers);
+    Serial.println("LED Driver settings selected");
 
-  int test[2];
-  test[0] = drivers[0].initialize();
-  test[1] = drivers[1].initialize();
-  if ((test[0] == IS31_TRANSFER_SUCCESS) && (test[1] == IS31_TRANSFER_SUCCESS)) {
-    Serial.println("LED DRIVERS CONFIGURED SUCCESSFULLY");
-  }
-  else Serial.println("LED DRIVER SETUP ERROR");
-  
-  //touch.initialize();
+    int test[2];
+    test[0] = drivers[0].initialize();
+    test[1] = drivers[1].initialize();
+    if ((test[0] == IS31_TRANSFER_SUCCESS) && (test[1] == IS31_TRANSFER_SUCCESS)) {
+        Serial.println("LED DRIVERS CONFIGURED SUCCESSFULLY");
+    }
+    else Serial.println("LED DRIVER SETUP ERROR");
+    
+    if (touch.initialize() == CAP1206_TRANSFER_SUCCESS) Serial.println("TOUCH SENSOR SUCCESSFULLY CONFIGURED");
+    else Serial.println("TOUCH SENSOR ERROR");
 
-  delay(2000);
+    delay(2000);
 }
 
 void loop() {
-  LEDfsm(ledFSMstates::BREATH, 0);
-  remap(drivers);
-  drivers[0].updateDuties();
-  drivers[1].updateDuties();
-  delay(10);
+    uint8_t buttons = 0;
+    touch.readSensors(&buttons);
+    LEDfsm(ledFSMstates::BREATH, buttons);
+
+    remap(drivers);
+    drivers[0].updateDuties();
+    drivers[1].updateDuties();
+    delay(10);
 }
