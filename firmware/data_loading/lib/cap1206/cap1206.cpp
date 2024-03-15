55cb41828e5a3c8867ecfd2c9c8166c23bdf40af
Savo Bajic <bajic.savo@gmail.com>
2024-03-15 04:51:10 -0400

    Raised thresholds from testing

--- firmware/data_loading/lib/cap1206/cap1206.cpp
+++ firmware/data_loading/lib/cap1206/cap1206.cpp
@@ -5,7 +5,7 @@
 const int CAP1206_TRANSFER_FAIL = -1;
 const int CAP1206_TRANSFER_SUCCESS = 0;
 
-uint8_t defaultThresholds[] = {40, 40, 40, 40, 10, 10};
+uint8_t defaultThresholds[] = {50, 50, 50, 50, 10, 10};
 
 /**
  * \brief Construct a new CAP1206 object
